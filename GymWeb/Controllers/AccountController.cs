b8029aa72c599d3ee9a533f078932d335331af6b
PBinh <tongphuocbinh123@gmail.com>
2026-07-21 21:17:55 +0700

    Đăng ký và Đăng nhập 2

--- GymWeb/Controllers/AccountController.cs
+++ GymWeb/Controllers/AccountController.cs
@@ -1,8 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using GymWeb.Models;
+using GymWeb.ViewModels;
 using Microsoft.AspNetCore.Identity;
 
-namespace ProjectWeb.Controllers // 
+namespace GymWeb.Controllers
 {
     public class AccountController : Controller
     {
@@ -41,11 +42,53 @@ namespace ProjectWeb.Controllers //
         return RedirectToAction("Index", "Home");
     }
         
-        public IActionResult Register()
-        {
-            return View();
-        }
+        [HttpGet]
+        public IActionResult Register() => View(new AccountCreateViewModel());
 
-        
+        [HttpPost]
+        public IActionResult Register(AccountCreateViewModel model)
+        {
+            // Người dùng tự đăng ký luôn là Member, không cho chọn Role
+            model.Role = "Member";
+
+            if (_context.Accounts.Any(a => a.Username == model.Username))
+            {
+                ModelState.AddModelError("Username", "Tên đăng nhập này đã tồn tại!");
+            }
+
+            if (string.IsNullOrWhiteSpace(model.RawPassword))
+            {
+                ModelState.AddModelError("RawPassword", "Mật khẩu không được để trống");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var account = new Account { Username = model.Username, Role = "Member" };
+            var hasher = new PasswordHasher<Account>();
+            account.PasswordHash = hasher.HashPassword(account, model.RawPassword);
+
+            _context.Accounts.Add(account);
+            _context.SaveChanges(); // Lưu để lấy AccountID tự sinh
+
+            var member = new Member
+            {
+                FullName = model.FullName,
+                PhoneNumber = model.PhoneNumber,
+                Email = model.Email,
+                Gender = model.Gender,
+                AccountID = account.AccountID
+            };
+            _context.Members.Add(member);
+            _context.SaveChanges();
+
+            // Tự động đăng nhập sau khi đăng ký thành công
+            HttpContext.Session.SetString("Username", account.Username);
+            HttpContext.Session.SetString("Role", account.Role);
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
